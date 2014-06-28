cb9294102a5c6a471b327fe59801b7d74b948f4d
Herb Miller Jr <herb@greatdayapproaches.net>
2014-06-27 21:35:58 -0400

    Successfully shown a window.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
-#include "SDL.h"
+#include "SDL2/SDL.h"
 
+/****\
+|
+\****/
 int main(int argc, char* args[])
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
+	
+	SDL_Window *win = SDL_CreateWindow("Hellow World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	
+	SDL_Delay(2000);
 	
 	SDL_Quit();
 	
 	return 0;
 }
-	
